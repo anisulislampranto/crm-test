54a3b4ba93a6b1303cf573a19ec4edb606b7a9f6
Anisul Islam <anisulislam.pranto@gmail.com>
2023-02-22 14:09:15 +0600

    Not resolved

--- blocks/index.ts
+++ blocks/index.ts
@@ -1,12 +1,14 @@
 import { CallToAction, Component as cta } from './CallToAction';
 import { Content, Component as content } from './Content';
 import { Image, Component as image } from './Image';
-import { Products, Component as products } from './Products/Products';
+import { Products, Component as products } from './Products';
+import { ImageCopy, Component as imageCopy } from './ImageCopy';
 
 export const collections = {
   CallToAction,
   Content,
   Image,
+  ImageCopy,
   Products,
 };
 
@@ -14,5 +16,6 @@ export const components = {
   cta,
   content,
   image,
+  imageCopy,
   products,
 };
